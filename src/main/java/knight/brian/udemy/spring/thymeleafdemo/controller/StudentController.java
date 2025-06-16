58ebd0b4c1b7b688b4197170767b4f77fcc34dfd
Brian Knight <knight.j.brian@gmail.com>
2025-06-16 15:26:38 -0600

    completed #196

--- src/main/java/knight/brian/udemy/spring/thymeleafdemo/controller/StudentController.java
+++ src/main/java/knight/brian/udemy/spring/thymeleafdemo/controller/StudentController.java
@@ -1,14 +1,20 @@
 package knight.brian.udemy.spring.thymeleafdemo.controller;
 
 import knight.brian.udemy.spring.thymeleafdemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
+
+    @Value("${countries}")
+    private List<String> countries;
 
     @GetMapping("/showStudentForm")
     public String showForm(Model model) {
@@ -18,6 +24,7 @@ public class StudentController {
 
         // add student object to the model
         model.addAttribute("student", theStudent);
+        model.addAttribute("countries", countries);
 
         return "student-form";
     }
